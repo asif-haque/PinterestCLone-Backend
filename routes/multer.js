9233de4bf412c4496d7261a543e1c56c0573e53b
Asif <asifhaque0505@gmail.com>
2024-03-29 22:27:19 +0530

    path

--- routes/multer.js
+++ routes/multer.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/images/uploads/");
+    cb(null, path.resolve("./public/images/uploads/"));
   },
   filename: function (req, file, cb) {
     const unique = Date.now();
